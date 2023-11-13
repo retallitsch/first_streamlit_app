ea667ed91c11026f6fb35cdb040db0575aab2361
retallitsch <149599300+retallitsch@users.noreply.github.com>
2023-11-13 15:32:14 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,10 @@ import streamlit as sl
 
 sl.title('My Parents New Healthy Diner')
 
+    
+
 sl.subheader('Breakfast Menu')
-sl.text('Omega 3 & Blueberry Oatmeal')
-sl.text('Kale, Spinach & Rocket Smoothie')
-sl.text('Hard-Boiled Free-Range Egg')
+sl.text('🥣 Omega 3 & Blueberry Oatmeal')
+sl.text('🥗 Kale, Spinach & Rocket Smoothie')
+sl.text('🐔 Hard-Boiled Free-Range Egg')
+sl.text('🥑🍞 Avocado Toast')
