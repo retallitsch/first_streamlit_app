3a3c9bf14ce065fdfd101b1b558113ef9af1f3ad
retallitsch <149599300+retallitsch@users.noreply.github.com>
2023-11-13 15:33:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,5 @@ sl.text('🥣 Omega 3 & Blueberry Oatmeal')
 sl.text('🥗 Kale, Spinach & Rocket Smoothie')
 sl.text('🐔 Hard-Boiled Free-Range Egg')
 sl.text('🥑🍞 Avocado Toast')
+
+sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
