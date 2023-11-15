645db7686fb6ff569fb6652aa7187803754e15ed
retallitsch <149599300+retallitsch@users.noreply.github.com>
2023-11-15 09:48:17 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,10 @@ sl.dataframe(fruits_to_show)
 sl.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
-# write your own comment -what does the next line do? 
+# write your own comment -what does the next line do?
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+
 # write your own comment - what does this do?
 sl.dataframe(fruityvice_normalized)
