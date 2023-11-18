982cfbc14d3cf8612afc591d2baae9ff18adaa7f
retallitsch <149599300+retallitsch@users.noreply.github.com>
2023-11-17 19:46:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as sl
 import pandas as pd
+import snowflake.connector
+
 
 # Display the table on the page.
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
